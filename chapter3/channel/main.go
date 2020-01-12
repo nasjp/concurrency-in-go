f707620ce06287d0a302510362b9e6c0a019f187
nas <nas.silver.bullet@gmail.com>
2020-01-12 21:43:37 +0900

    :+1: Add channel

---
+++ chapter3/channel/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"sync"
+)
+
+func main() {
+	// simpleChannel()
+	// simpleBroadcast()
+	// bufChan()
+	ownerShip()
+}
+
+func simpleChannel() {
+	intStream := make(chan int)
+	go func() {
+		defer close(intStream)
+		for i := 1; i <= 5; i++ {
+			intStream <- i
+		}
+	}()
+
+	for integer := range intStream {
+		fmt.Printf("%v ", integer)
+	}
+}
+
+func simpleBroadcast() {
+	begin := make(chan interface{})
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			<-begin
+			fmt.Printf("%v has begun\n", i)
+		}(i)
+	}
+	fmt.Println("Unblocking goroutines...")
+	close(begin)
+	wg.Wait()
+}
+
+func bufChan() {
+	var stdoutBuff bytes.Buffer
+	defer stdoutBuff.WriteTo(os.Stdout)
+
+	intStream := make(chan int, 4)
+	go func() {
+		defer close(intStream)
+		defer fmt.Fprintln(&stdoutBuff, "Producer Done.")
+		for i := 0; i < 5; i++ {
+			fmt.Fprintf(&stdoutBuff, "Sending: %d\n", i)
+			intStream <- i
+		}
+	}()
+
+	for integer := range intStream {
+		fmt.Fprintf(&stdoutBuff, "Received %v.\n", integer)
+	}
+}
+
+func ownerShip() {
+	chanOwner := func() <-chan int {
+		resultStream := make(chan int, 5)
+		go func() {
+			defer close(resultStream)
+			for i := 0; i <= 5; i++ {
+				resultStream <- i
+			}
+		}()
+		return resultStream
+	}
+
+	resultStream := chanOwner()
+	for result := range resultStream {
+		fmt.Printf("Received: %d\n", result)
+	}
+	fmt.Println("Done receiving!")
+}
