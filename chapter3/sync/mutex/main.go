b3b09981599a953d1f5f8670915ad0b29c412807
nas <nas.silver.bullet@gmail.com>
2020-01-12 16:38:47 +0900

    :+1: Add sync mutext

---
+++ chapter3/sync/mutex/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"sync"
+	"text/tabwriter"
+	"time"
+)
+
+func main() {
+	// arithmetic()
+	rwMutex()
+}
+
+func arithmetic() {
+	var count int
+	var lock sync.Mutex
+
+	increment := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count++
+		fmt.Printf("Incrementing: %d\n", count)
+	}
+
+	decrement := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count--
+		fmt.Printf("Decrementing: %d\n", count)
+	}
+
+	var arithmetic sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			increment()
+		}()
+	}
+
+	for i := 0; i < 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			decrement()
+		}()
+	}
+
+	arithmetic.Wait()
+
+	fmt.Println("Arithmetic complete.")
+}
+
+func rwMutex() {
+	producer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		for i := 5; i > 0; i-- {
+			l.Lock()
+			l.Unlock()
+			time.Sleep(1)
+		}
+	}
+
+	observer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		l.Lock()
+		defer l.Unlock()
+	}
+
+	test := func(count int, mutex, rwMutex sync.Locker) time.Duration {
+		var wg sync.WaitGroup
+		wg.Add(count + 1)
+		beginTestTime := time.Now()
+		go producer(&wg, mutex)
+		for i := count; i > 0; i-- {
+			go observer(&wg, rwMutex)
+		}
+
+		wg.Wait()
+		return time.Since(beginTestTime)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 0, 1, 2, ' ', 0)
+	defer tw.Flush()
+
+	var m sync.RWMutex
+	fmt.Fprintf(tw, "Readers\tRWMutext\tMutex\n")
+
+	for i := 0; i < 20; i++ {
+		count := int(math.Pow(2, float64(i)))
+		fmt.Fprintf(
+			tw,
+			"%d\t%v\t%v\n",
+			count,
+			test(count, &m, m.RLocker()),
+			test(count, &m, &m),
+		)
+	}
+}
